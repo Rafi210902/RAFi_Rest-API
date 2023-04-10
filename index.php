7d6a80c0674c965f77243da2f4a17950b8d3aaea
Rafi210902 <102600434+Rafi210902@users.noreply.github.com>
2023-04-10 09:39:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,67 @@
-<?php
-
-function http_request($url)
-{
-  // curl adalah library untuk mengambil dan mengirim data melalui URL
-  // curl_init (Inisialisasi)
-  $ch = curl_init();
-
-  // curl_setopt (set Option)
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-  // curl_exec (eksekusi)
-  $output = curl_exec($ch);
-
-  // curl_close (tutup curl)
-  curl_close($ch);
-  return $output;
-}
-
-$api = http_request("https://api.steinhq.com/v1/storages/642a1f71eced9b09e9c76336/20a2");
-$data = json_decode($api, TRUE);
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="style.css">
+    <title>Mahasiswa TI.20.A.2</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
-  <h1>Daftar Nama Mahasiswa</h1>
-  <table>
-    <tr>
-      <td>NO. </td>
-      <td>NIM</td>
-      <td>Nama</td>
-    </tr>
-    <?php foreach ($data as $dt) : ?>
-    <tr>
-      <td>
-        <?= $dt["NO"]; ?>
-      </td>
-      <td><?= $dt["NIM"]; ?></td>
-      <td><?= $dt["Nama"]; ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </table>
+    <?php
+    $linksPerPage = 10;
+    $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+    $data = file_get_contents('https://api.steinhq.com/v1/storages/642110d4eced9b09e9c62384/20A2');
+
+    $links = json_decode($data, true);
+    $totalLinks = count($links);
+    $totalPages = ceil($totalLinks / $linksPerPage);
+
+    $currentLinks = array_slice($links, ($page - 1) * $linksPerPage, $linksPerPage);
+    ?>
+
+    <div class="container mt-5">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>NIM</th>
+                    <th>Nama</th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>4</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($currentLinks as $index => $link) { ?>
+                    <tr>
+                        <td><?php echo (($page - 1) * $linksPerPage + $index + 1); ?></td>
+                        <td><?php echo $link['NIM']; ?></td>
+                        <td><?php echo $link['Nama']; ?></td>
+                        <td><?php echo $link['1']; ?></td>
+                        <td><?php echo $link['2']; ?></td>
+                        <td><?php echo $link['3']; ?></td>
+                        <td><?php echo $link['4']; ?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+
+        <nav>
+            <ul class="pagination">
+                <?php for ($i = 1; $i <= $totalPages; $i++) { ?>
+                    <li class="page-item<?php echo ($i == $page ? ' active' : ''); ?>">
+                        <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </nav>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
